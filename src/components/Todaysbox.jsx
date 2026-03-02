e27286e52a39bac4989e28e1387c4a2c051d4c3d
raul4500 <raul425borges@gmail.com>
2026-03-01 22:20:13 -0300

    buttons fixed

--- src/components/Todaysbox.jsx
+++ src/components/Todaysbox.jsx
@@ -1,19 +1,22 @@
 import './Todaysbox.css'
 
-function TodaysBox(){
+function TodaysBox() {
     return (
         <div className="todaysbox">
-            <div className="muscles">
-                PEITO | TRÍCEPS
-            </div>
             <div className="buttons">
-            <button>
-                S T A R T
-            </button>
-            <button>
-                S T A R T
-            </button>
+                <div className="muscles">
+                    CHEST | TRICEPS
+                </div>
+                <button className="viewmore">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+                    </svg>
+                </button>
             </div>
+            <button>
+                <a href="teste">S T A R T</a>
+            </button>
+
         </div>
     )
 };
